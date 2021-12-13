9ee1480b423bedb6aa91cafb6fad4a6b67525185
Everytick-Juns <6_5ee@naver.com>
2021-12-13 22:26:49 +0900

    xvwa 수정

--- xvwa/sidepanel.php
+++ xvwa/sidepanel.php
@@ -1,28 +1,28 @@
 <div class="list-group">
     <a href="#" class="list-group-item active">Setup</a>
-    <a href="/xvwa/" class="list-group-item">Home</a>
-    <a href="/xvwa/instruction.php" class="list-group-item">Instructions</a>
-    <a href="/xvwa/setup/" class="list-group-item">Setup / Reset</a>
+    <a href="/bufs/xvwa/" class="list-group-item">Home</a>
+    <a href="/bufs/xvwa/instruction.php" class="list-group-item">Instructions</a>
+    <a href="/bufs/xvwa/setup/" class="list-group-item">Setup / Reset</a>
 </div>
 <div class="list-group">
     <a href="#" class="list-group-item active">Attacks</a>
-    <a href="/xvwa/vulnerabilities/sqli/" class="list-group-item">SQL Injection</a>
-    <a href="/xvwa/vulnerabilities/sqli_blind/" class="list-group-item">SQL Injection (Blind)</a>
-    <a href="/xvwa/vulnerabilities/cmdi/" class="list-group-item">OS Command Injection</a>
-    <a href="/xvwa/vulnerabilities/xpath/" class="list-group-item">XPATH Injection</a>
-    <a href="/xvwa/vulnerabilities/formula_injection/" class="list-group-item">Formula Injection</a>
-    <a href="/xvwa/vulnerabilities/php_object_injection/" class="list-group-item">PHP Object Injection</a>
-    <a href="/xvwa/vulnerabilities/fileupload/" class="list-group-item">Unrestricted File Upload</a>
-    <a href="/xvwa/vulnerabilities/reflected_xss/" class="list-group-item">XSS - Reflected</a>
-    <a href="/xvwa/vulnerabilities/stored_xss/" class="list-group-item">XSS - Stored</a>
-    <a href="/xvwa/vulnerabilities/dom_xss/" class="list-group-item">XSS - DOM Based</a>
-    <a href="/xvwa/vulnerabilities/ssrf_xspa/" class="list-group-item">SSRF / XSPA</a>
-    <a href="/xvwa/vulnerabilities/fi/" class="list-group-item">File Inclusion</a>
-    <a href="/xvwa/vulnerabilities/sessionflaws/" class="list-group-item">Session Flaws</a>
-    <a href="/xvwa/vulnerabilities/idor/" class="list-group-item">Insecure Direct Object Reference</a>
-    <a href="/xvwa/vulnerabilities/missfunc/" class="list-group-item">Missing Functional Access Control</a>
-    <a href="/xvwa/vulnerabilities/csrf/" class="list-group-item">CSRF</a>
-    <a href="/xvwa/vulnerabilities/crypto/" class="list-group-item">Cryptography</a>
-    <a href="/xvwa/vulnerabilities/redirect/" class="list-group-item">Redirects & Forwards</a>
-    <a href="/xvwa/vulnerabilities/ssti/" class="list-group-item">Server Side Template Injection</a>
+    <a href="/bufs/xvwa/vulnerabilities/sqli/" class="list-group-item">SQL Injection</a>
+    <a href="/bufs/xvwa/vulnerabilities/sqli_blind/" class="list-group-item">SQL Injection (Blind)</a>
+    <a href="/bufs/xvwa/vulnerabilities/cmdi/" class="list-group-item">OS Command Injection</a>
+    <a href="/bufs/xvwa/vulnerabilities/xpath/" class="list-group-item">XPATH Injection</a>
+    <a href="/bufs/xvwa/vulnerabilities/formula_injection/" class="list-group-item">Formula Injection</a>
+    <a href="/bufs/xvwa/vulnerabilities/php_object_injection/" class="list-group-item">PHP Object Injection</a>
+    <a href="/bufs/xvwa/vulnerabilities/fileupload/" class="list-group-item">Unrestricted File Upload</a>
+    <a href="/bufs/xvwa/vulnerabilities/reflected_xss/" class="list-group-item">XSS - Reflected</a>
+    <a href="/bufs/xvwa/vulnerabilities/stored_xss/" class="list-group-item">XSS - Stored</a>
+    <a href="/bufs/xvwa/vulnerabilities/dom_xss/" class="list-group-item">XSS - DOM Based</a>
+    <a href="/bufs/xvwa/vulnerabilities/ssrf_xspa/" class="list-group-item">SSRF / XSPA</a>
+    <a href="/bufs/xvwa/vulnerabilities/fi/" class="list-group-item">File Inclusion</a>
+    <a href="/bufs/xvwa/vulnerabilities/sessionflaws/" class="list-group-item">Session Flaws</a>
+    <a href="/bufs/xvwa/vulnerabilities/idor/" class="list-group-item">Insecure Direct Object Reference</a>
+    <a href="/bufs/xvwa/vulnerabilities/missfunc/" class="list-group-item">Missing Functional Access Control</a>
+    <a href="/bufs/xvwa/vulnerabilities/csrf/" class="list-group-item">CSRF</a>
+    <a href="/bufs/xvwa/vulnerabilities/crypto/" class="list-group-item">Cryptography</a>
+    <a href="/bufs/xvwa/vulnerabilities/redirect/" class="list-group-item">Redirects & Forwards</a>
+    <a href="/bufs/xvwa/vulnerabilities/ssti/" class="list-group-item">Server Side Template Injection</a>
 </div>
